3f48b96d7a02f4ed8b58edcf5eb09e7615140ef1
miruji <miruji@protonmail.com>
2024-12-25 23:27:30 +0300

    Fixed bug

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@
 #![allow(non_camel_case_types)]
 
 use std::fs::File;
-use std::io::{Read, Write};
 use std::env;
-use std::path::Path;
 use brotli2::write::BrotliEncoder;
 
 fn compress(inputPath: &str, outputPath: &str) -> std::io::Result<()> 
@@ -16,10 +14,10 @@ fn compress(inputPath: &str, outputPath: &str) -> std::io::Result<()>
 
   // Создаем выходной файл для сжатых данных
   let outputFile: File = File::create(outputPath)?;
-  let mut output_writer = std::io::BufWriter::new(outputFile);
+  let outputWriter = std::io::BufWriter::new(outputFile);
 
   // Создаем BrotliEncoder с уровнем сжатия 5
-  let mut encoder: BrotliEncoder = BrotliEncoder::new(output_writer, 5);
+  let mut encoder: BrotliEncoder<std::io::BufWriter<File>> = BrotliEncoder::new(outputWriter, 5);
 
   // Копируем данные из исходного файла в сжимающий поток
   std::io::copy(&mut inputReader, &mut encoder)?;
