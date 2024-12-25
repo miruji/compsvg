cb4cf017ccd5b9199f4ce4ab13e727463427327f
miruji <miruji@protonmail.com>
2024-12-25 14:01:24 +0300

    Updated to brotli

--- src/main.rs
+++ src/main.rs
@@ -6,41 +6,50 @@ use std::fs::File;
 use std::io::{Read, Write};
 use std::env;
 use std::path::Path;
-use zstd::stream::Encoder;
+use brotli2::write::BrotliEncoder;
 
 fn compress(inputPath: &str, outputPath: &str) -> std::io::Result<()> 
 {
-  // Открываем файл с .svg
-  let mut inputFile: File = File::open(inputPath)?;
-  let mut buffer: Vec<u8> = Vec::new();
-  inputFile.read_to_end(&mut buffer)?;
+  // Открываем исходный SVG-файл
+  let inputFile: File = File::open(inputPath)?;
+  let mut inputReader = std::io::BufReader::new(inputFile);
 
-  // Создаем выходной файл
+  // Создаем выходной файл для сжатых данных
   let outputFile: File = File::create(outputPath)?;
+  let mut output_writer = std::io::BufWriter::new(outputFile);
 
-  // Используем zstd для сжатия
-  let mut encoder: Encoder<File> = Encoder::new(outputFile, 0)?; // Уровень сжатия 0 (по умолчанию)
-  encoder.write_all(&buffer)?;
+  // Создаем BrotliEncoder с уровнем сжатия 5
+  let mut encoder: BrotliEncoder = BrotliEncoder::new(output_writer, 5);
+
+  // Копируем данные из исходного файла в сжимающий поток
+  std::io::copy(&mut inputReader, &mut encoder)?;
+
+  // Завершаем сжатие
   encoder.finish()?;
 
   println!("[Ok] Compressed to [{}]", outputPath);
   Ok(())
 }
 
+// for bash:
+//for file in 111/*.svg; do 
+//  cargo run --release -- "$file"
+//done
+
 fn main() 
 {
   let args: Vec<String> = env::args().collect();
 
-  if args.len() != 2
+  if args.len() != 2 
   {
-    eprintln!("[Help] compsvg <input_file>");
+    eprintln!("[Help] compress_svg <inputFile>");
     return;
   }
 
-  let inputPath: &str = &args[1];
-  
-  // Генерация имени для выходного файла с заменой расширения на .zst
-  let outputPath: String = format!("{}", Path::new(inputPath).with_extension("zst").display());
+  let inputPath = &args[1];
+
+  // Генерация имени для выходного файла с заменой расширения на .svg.br
+  let outputPath: String = format!("{}.br", inputPath);
 
   match compress(inputPath, &outputPath) 
   {
